3654bdb7a3efcf7a8ba34d53919bd47c1dbd4aaf
BurlakovaKA <formerfox@yandex.ru>
2025-09-02 20:13:45 +0300

    Create TelegramBot

--- TelegramBot/Program.cs
+++ TelegramBot/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using TelegramBot.Services;
+
+Console.WriteLine("=== Telegram Text Bot ===");
+
+// Токен бота 
+string botToken = "8360825158:AAF4xAP9vkADj0YGGnCZptdF0j82-TmuioU";
+
+try
+{
+    // Создаем и запускаем бота
+    var botService = new BotService(botToken);
+    botService.Start();
+
+    // Ждем бесконечно, чтобы приложение не закрылось
+    Console.WriteLine("Нажмите Ctrl+C для остановки бота");
+    await Task.Delay(Timeout.Infinite);
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Критическая ошибка: {ex.Message}");
+}
---
+++ TelegramBot/Services/TextProcessor.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace TelegramBot.Services
+{
+    /// Класс для обработки текстовых данных
+    /// Выполняет две основные функции: подсчет суммы чисел и анализ текста
+    public class TextProcessor
+    {
+
+        /// Вычисляет сумму всех чисел в тексте
+        /// Ищет числа с помощью регулярного выражения и суммирует их
+        public int CalculateSum(string text)
+        {
+            // Регулярное выражение для поиска чисел (включая отрицательные и десятичные)
+            var matches = Regex.Matches(text, @"-?\d+\.?\d*");
+
+            int sum = 0;
+            foreach (Match match in matches)
+            {
+                // Парсим каждое найденное число и добавляем к сумме
+                if (double.TryParse(match.Value, out double number))
+                {
+                    sum += (int)number;
+                }
+            }
+            return sum;
+        }
+
+        /// Анализирует текст и возвращает статистику
+        /// Подсчитывает слова, предложения и общее количество символов
+        public (int words, int sentences, int characters) AnalyzeText(string text)
+        {
+            if (string.IsNullOrEmpty(text))
+                return (0, 0, 0);
+
+            // Подсчет слов: разделяем текст по пробелам и удаляем пустые элементы
+            int words = text.Split(new[] { ' ', '\t', '\n', '\r' },
+                                StringSplitOptions.RemoveEmptyEntries).Length;
+
+            // Подсчет предложений: ищем точки, восклицательные и вопросительные знаки
+            int sentences = Regex.Matches(text, @"[.!?]+").Count;
+
+            // Подсчет символов: просто длина строки
+            int characters = text.Length;
+
+            return (words, sentences, characters);
+        }
+    }
+}
